f757a60911ba72090ac9ef3c3dcb1be60d683a7c
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 16:47:18 +0200

    ADD ejercicios day 2 bloque 1-1

---
+++ DAY 3/js1/exercise1-1.js
@@ -0,0 +1,11 @@
+// 1-. Declare las siguientes variables; firstName, lastName, country, city, age, isMarried, year y asignar un valor, use el operador typeof para verificar diferentes tipos de datos.
+
+const firstName = typeof 'Jorge'
+const lastName = typeof null
+const country = typeof 'España'
+const city = typeof 'Navas del Madroño'
+const age = typeof 30
+const isMarried = typeof null
+const year = typeof undefined
+
+console.log (' firstName: ' + firstName,'\n','lastName: ' + lastName,'\n','country: ' + country,' \n','city: ' + city,'\n','age: + ' + age,'\n','isMarried: ' +  isMarried,'\n', 'year: ' + year,'\n',)
---
+++ DAY 3/js1/exercise1-2.js
@@ -0,0 +1,6 @@
+// 2-. Verifique si typeof '10' es igual a 10
+
+const text1 = typeof'10'
+const number1 = 10
+
+console.log(text1 = number1)
