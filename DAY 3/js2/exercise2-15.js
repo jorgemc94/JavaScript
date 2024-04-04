eb359a397ce4823eb380a309d65d7dc03dc4d374
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 23:45:44 +0200

    ADD ejercicios day 3 bloque 3-

--- DAY 3/js2/exercise2-15.js
+++ DAY 3/js2/exercise2-15.js
@@ -1,10 +1,10 @@
 // 15-. Cree un formato de hora legible por humanos usando el objeto Date.
 
-const year = new Date().getFullYear(); // devuelve años
-const month = new Date().getMonth() + 1; // devuelve meses(0 - 11)
-const date = new Date().getDate(); // devuelve días (1 - 31)
-const hours = new Date().getHours(); // devuelve horas (0 - 23)
-const minutes = new Date().getMinutes(); // devuelve minutos (0 -59)
+const year = new Date().getFullYear()
+const month = new Date().getMonth() + 1
+const date = new Date().getDate()
+const hours = new Date().getHours()
+const minutes = new Date().getMinutes()
 
 //YYYY-MM-DD HH:mm
 console.log('formato YYYY-MM-DD HH:mm\n',year,'-',month,'-',date, ' ',hours,':',minutes ) 
---
+++ DAY 3/js3/exercise3-1.js
@@ -0,0 +1,13 @@
+// 1-. Cree un formato de hora legible por humanos usando el objeto Date. La hora y el minuto deben ser siempre dos dígitos (7 horas deben ser 07 y 5 minutos deben ser 05) YYY-MM-DD HH:mm
+const time = new Date()
+const year = time.getFullYear()
+const month = time.getMonth()
+const date = time.getDate() 
+const hours = time.getHours() 
+const minutes = time.getMinutes()
+
+
+if (hours <= 9 || minutes <= 9) 
+    console.log(`${year}-${month}-${date} 0${hours}:0${minutes}`)
+else 
+    console.log(`${year}-${month}-${date} ${hours}:${minutes}`)
