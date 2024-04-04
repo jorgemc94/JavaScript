c6378d1482b8cb288faedd9be82e8fafa7fb0725
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 19:00:44 +0200

    ADD ejercicios day 3 bloque error

--- DAY 3/js2/exercise2-1.js
+++ DAY 3/js2/exercise2-1.js
@@ -1,8 +1,8 @@
 //1-. Escriba un script que solicite al usuario que ingrese la base y la altura del triángulo y calcule el área de un triángulo (área = 0,5 x b x h).
 
-const base = prompt ('Ingrese la base: ')
-const altura = prompt ('Ingrese la altura: ')
+const baseT = prompt ('Ingrese la base: ')
+const alturaT = prompt ('Ingrese la altura: ')
 
-const area = 0.5 * parseInt(base) * parseInt(altura)
+const areaT = 0.5 * parseInt(baseT) * parseInt(alturaT)
 
-console.log ('El área del triángulo es: ' + area)
+console.log ('El área del triángulo es: ' + areaT)
--- DAY 3/js2/exercise2-2.js
+++ DAY 3/js2/exercise2-2.js
@@ -1,8 +1,8 @@
 //2-. Escriba un script que solicite al usuario que ingrese el lado a, el lado b y el lado c del triángulo y calcule el perímetro del triángulo (perímetro = a + b + c)
 
-const ladoA = prompt('Introduce el lado A: ')
-const ladoB = prompt('Introduce el lado B: ')
-const ladoC = prompt('Introduce el lado C: ')
+const ladoAT = prompt('Introduce el lado A: ')
+const ladoBT = prompt('Introduce el lado B: ')
+const ladoCT = prompt('Introduce el lado C: ')
 
-const ladoS = parseInt(ladoA) + parseInt(ladoB) + parseInt(ladoC)
-console.log('El perímetro del triángulo es: ' + ladoS)
+const ladoST = parseInt(ladoAT) + parseInt(ladoBT) + parseInt(ladoCT)
+console.log('El perímetro del triángulo es: ' + ladoST)
