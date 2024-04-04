8369ad92ea0048b0518349b1a50a5422d6f93c74
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 20:19:26 +0200

    ADD ejercicios day 3 bloque 2-5 y 2-7

---
+++ DAY 3/js2/exercise2-5.js
@@ -0,0 +1,10 @@
+// 5-. Calcule la pendiente, la intersección X y la intersección Y de y = 2x -2
+
+const xi_5 = 1
+const xii_5 = 0
+const yi_5 = -2
+const yii_5 = 0
+
+const pendiente5 = (xii_5 - xi_5) / (yii_5 - yi_5)
+
+console.log('La pendiente es: ' + pendiente5)
---
+++ DAY 3/js2/exercise2-7.js
@@ -0,0 +1,4 @@
+// 7-. Compare la pendiente de las dos preguntas anteriores.
+
+const pendienteT = parseFloat(pendiente5) - parseFloat(pendiente6)
+console.log ('La diferencia de pendiente entre ' , pendiente5 , 'y ' , pendiente6 ,' es: ' , pendienteT)
