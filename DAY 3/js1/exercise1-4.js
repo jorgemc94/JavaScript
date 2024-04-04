35697badb2dc2a09996d0554d11f2ecdf8131ec2
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 17:06:48 +0200

    ADD ejercicios day 3 bloque 1-4

---
+++ DAY 3/js1/exercise1-4.js
@@ -0,0 +1,7 @@
+//4-. Verifique cualquier valor booleano true o false.
+
+//Escriba tres declaraciones de JavaScript que proporcionen un valor verdadero.
+
+console.log('Las declaraciones 4 > 1 && "a" === a && "10" === "10" son: ' , 4 > 1 && "a" === "a" && '10' === '10')
+//Escriba tres declaraciones de JavaScript que proporcionen un valor falso.
+console.log('Las declaraciones 4 > 1 && "a" === a && "10" === 10 son: ' , 4 > 1 && "a" === "a" && '10' === 10)
