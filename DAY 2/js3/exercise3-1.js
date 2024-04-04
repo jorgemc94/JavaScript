501128c523e663caeb4ee4ed0811dfe135613ce2
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 12:41:29 +0200

    ADD ejercicios day 2 bloque 3-1

---
+++ DAY 2/js3/exercise3-1.js
@@ -0,0 +1,5 @@
+// 3-. 'El amor es lo mejor que hay en este mundo. Algunos encontraron su amor y algunos todavía están buscando su amor. Cuente el número de palabras amor en esta oración.
+
+let tex1 = 'El amor es lo mejor que hay en este mundo. Algunos encontraron su amor y algunos todavía están buscando su amor.'
+
+console.log(tex1.match('amor'))
---
+++ DAY 2/js3/exercise3-2.js
@@ -0,0 +1,5 @@
+// 2-. Usa match() para contar el número de todos los porque en la siguiente oración:'No puedes terminar una oración con porque porque porque es una conjunción'
+
+let text2 = 'No puedes terminar una oración con porque porque porque es una conjunción'
+
+console.log(text2.match('porque'))
