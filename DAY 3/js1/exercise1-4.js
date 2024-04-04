3b1e3699948758bc00914e48f129af38acc49c96
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 17:53:45 +0200

    ADD ejercicios day 3 bloque 1-6

--- DAY 3/js1/exercise1-4.js
+++ DAY 3/js1/exercise1-4.js
@@ -4,4 +4,4 @@
 
 console.log('Las declaraciones 4 > 1 && "a" === a && "10" === "10" son: ' , 4 > 1 && "a" === "a" && '10' === '10')
 //Escriba tres declaraciones de JavaScript que proporcionen un valor falso.
-console.log('Las declaraciones 4 > 1 && "a" === a && "10" === 10 son: ' , 4 > 1 && "a" === "a" && '10' === 10)
+console.log('Las declaraciones 4 > 1 || "a" === a || "10" === 10 son: ' , 4 > 1 || "a" === "a" || '10' === 10)
---
+++ DAY 3/js1/exercise1_6.js
@@ -0,0 +1,58 @@
+//Calcule primero el resultado de las siguientes expresiones sin usar console.log(). Después de decidir el resultado, confirmelo usando console.log()
+
+//4 > 3 && 10 < 12
+
+const i_6 = 4 > 3 && 10 < 12
+console.log ('El resultado de 4 > 3 && 10 < 12 es: ' + i_6)
+
+//4 > 3 && 10 > 12
+
+const ii_6 = 4 > 3 && 10 > 12
+console.log ('El resultado de 4 > 3 && 10 > 12 es: ' + ii_6)
+
+//4 > 3 || 10 < 12
+
+const iii_6 = 4 > 3 || 10 < 12
+console.log ('El resultado de 4 > 3 || 10 < 12 es: ' + iii_6)
+
+//4 > 3 || 10 > 12
+
+const iv_6 = 4 > 3 || 10 > 12
+console.log('El resultado de 4 > 3 || 10 > 12 es: ' + iv_6)
+
+//!(4 > 3)
+
+const v_6 = !(4 > 3)
+console.log ('El resultado de !(4 > 3) es: ' + v_6)
+
+//!(4 < 3)
+
+const vi_6 = !(4 < 3)
+console.log ('El resultado de !(4 < 3) es: ' + vi_6)
+
+//!(false)
+
+const vii_6 = !(false)
+console.log ('El resultado de !(false) es: ' + vii_6)
+
+//!(4 > 3 && 10 < 12)
+
+const viii_6 = !(4 > 3 && 10 < 12)
+console.log ('El resultado de !(4 > 3 && 10 < 12)) es: ' + viii_6)
+
+//!(4 > 3 && 10 > 12)
+
+const ix_6 = !(4 > 3 && 10 > 12)
+console.log ('El resultado de !(4 > 3 && 10 > 12)) es: ' + ix_6)
+
+//!(4 === '4')
+
+const x_6 = !(4 === '4')
+console.log ('El resultado de !(4 === "4")) es: ' + x_6)
+
+//No hay 'on' tanto en dragon como en python
+
+const text6_i = 'dragon'
+const text6_ii = 'python'
+const xi_6 = text6_i.includes('on') !== text6_ii.includes('on')
+console.log('El resultado de text6_i.includes("on") !== text6_ii.includes("on") es: ' + xi_6)
