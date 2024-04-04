4719a663976035b97c16d81bac716f1b943c1233
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 17:35:44 +0200

    ADD ejercicios day 3 bloque 1-5

---
+++ DAY 3/js1/exercise1-5.js
@@ -0,0 +1,64 @@
+//Calcule primero el resultado de la siguiente expresión de comparación sin usar console.log(). Después de decidir el resultado, confirmelo usando console.log()
+
+//4 > 3
+
+const i_5 = 4 > 3
+console.log ('El resultado de 4 > 3 es: ' + i_5)
+
+//4 >= 3
+
+const ii_5 = 4 >= 3
+console.log ('El resultado de 4 >= 3 es: ' + ii_5)
+
+//4 < 3
+
+const iii_5 = 4 < 3
+console.log ('El resultado de 4 < 3 es: ' + iii_5)
+
+
+//4 <= 3
+
+const iv_5 = 4 <= 3
+console.log ('El resultado de 4 <= 3 es: ' + iv_5)
+
+//4 == 4
+
+const v_5 = 4 == 4
+console.log ('El resultado de 4 == 4 es: ' + v_5)
+
+//4 === 4
+
+const vi_5 = 4 === 4
+console.log ('El resultado de 4 === 4 es: ' + vi_5)
+
+//4 != 4
+
+const vii_5 = 4 != 4
+console.log ('El resultado de 4 != 4 es: ' + vii_5)
+
+//4 !== 4
+
+const viii_5 = 4 !== 4
+console.log ('El resultado de 4 !== 4 es: ' + viii_5)
+
+//4 != '4'
+
+const ix_5 = 4 != '4'
+console.log ('El resultado de 4 != "4" es: ' + ix_5)
+
+//4 == '4'
+
+const x_5 = 4 == '4'
+console.log ('El resultado de 4 == "4" es: ' + x_5)
+
+//4 === '4'
+const xi_5 = 4 === '4'
+console.log ('El resultado de 4 === "4" es: ' + xi_5)
+
+//Encuentre la longitud de Python y jargon y haga una declaración de comparación falsa.
+
+const text5_i = 'Python'
+const text5_ii = 'jargon'
+
+const xii_5 = text5_i.length !== text5_ii.length
+console.log('El resultado de text5_i(Python).length !== text5_ii(jargon).length es ' + xii_5)
