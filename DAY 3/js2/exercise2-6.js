3e482b05371cdef628f18f2c60273e9e20ee5579
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 19:38:24 +0200

    ADD ejercicios day 3 bloque 2-6

--- DAY 3/js2/exercise2-6.js
+++ DAY 3/js2/exercise2-6.js
@@ -1,2 +1,10 @@
 //6-. La pendiente es m = (y2-y1)/(x2-x1). Encuentra la pendiente entre el punto (2, 2) y el punto (6,10)
 
+const xi_6 = 2
+const xii_6 = 6
+const yi_6 = 2
+const yii_6 = 10
+
+const pendiente6 = (xii_6 - xi_6) / (yii_6 - yi_6)
+
+console.log('La pendiente es: ' + pendiente6)
