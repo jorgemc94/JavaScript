a4fb811fb231b4170ca1d0e8f8ff873e7d06a080
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-04 18:48:40 +0200

    ADD ejercicios day 3 bloque 2-1

---
+++ DAY 3/js2/exercise2-1.js
@@ -0,0 +1,8 @@
+//1-. Escriba un script que solicite al usuario que ingrese la base y la altura del triángulo y calcule el área de un triángulo (área = 0,5 x b x h).
+
+const base = prompt ('Ingrese la base: ')
+const altura = prompt ('Ingrese la altura: ')
+
+const area = 0.5 * base * altura
+
+console.log ('El área del triángulo es: ' + area)
