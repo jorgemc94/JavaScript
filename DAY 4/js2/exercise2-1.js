093251bcdb7a35884ba6780d4ff5dfe621d330ad
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-05 12:09:36 +0200

    ADD ejercicios day 4 bloque 2-1

---
+++ DAY 4/js2/exercise2-1.js
@@ -0,0 +1,25 @@
+//1-. Escriba un código que pueda calificar a los estudiantes de acuerdo con sus puntajes:
+
+/*
+90-100, A
+70-89, B
+60-69, C
+50-59, D
+0-49, F
+*/
+
+const nota = prompt('Introduce la nota: ')
+
+if (nota <=49) {
+    console.log('Tu notas es: ' + nota + ' F')
+} else if (nota <= 59){
+    console.log('Tu notas es: ' + nota + ' D')
+} else if (nota <=69) {
+    console.log('Tu notas es: ' + nota + ' C')
+} else if (nota <= 89) {
+    console.log('Tu notas es: ' + nota + ' B')
+} else if ( nota <=100) {
+    console.log('Tu notas es: ' + nota + ' A')
+} else { (nota >100) 
+    alert('Nota erronea')
+}
