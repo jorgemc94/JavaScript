3aa5284c077aea1f3ed9689ce612f8b7c9f35b8e
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-10 12:30:47 +0200

    ADD day 5 exercise 1-6 1-7

---
+++ DAY 5/js1/exercise1-6.js
@@ -0,0 +1,3 @@
+//6-. Declare un variable array de nombre itCompanies y asignarles valores iniciales Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon.
+
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
---
+++ DAY 5/js1/exercise1-7.js
@@ -0,0 +1,2 @@
+//7-. Imprima el array usando console.log().
+console.log(itCompanies)
