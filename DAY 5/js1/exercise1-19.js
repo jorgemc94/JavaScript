4b11f844cb1fbdec2871eabfa766bbb2bd95b8f8
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-10 13:53:10 +0200

    ADD day 5 exercise 1-19

---
+++ DAY 5/js1/exercise1-19.js
@@ -0,0 +1,5 @@
+//19-. Cortar la empresa o empresas intermedias de TI del array
+
+const itCompaniesMedium = itCompanies.length / 2 //numero es 3.5
+//console.log(itCompaniesMedium)
+console.log(itCompanies.slice(3,4))
