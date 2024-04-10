bf3e6c719a331a9819d6a90e2128d1c91d9e1401
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-10 15:22:57 +0200

    ADD day 5 exercise 2-3

---
+++ DAY 5/js2/exercise2-3.js
@@ -0,0 +1,17 @@
+//3-. En el siguiente carrito de compras agregue, elimine, edite artículos
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+
+//Agregue 'Meat' al comienzo de su carrito de compras si aún no se ha agregado
+let shoppingCart1= shoppingCart.unshift('Meat');
+console.log(shoppingCart1)
+
+//Agregue 'Sugar' al final de su carrito de compras si aún no se ha agregado
+let shoppingCart2 = shoppingCart.push('Sugar')
+console.log(shoppingCart2)
+
+//Elimine 'Honey' si es alérgico a la miel (honey)
+
+
+//Modificar Tea a 'Green Tea'
+ 
