f9e735396262d65d24a89bd317bf15de9ec610e9
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-10 13:21:32 +0200

    ADD day 5 exercise 1-12

---
+++ DAY 5/js1/exercise1-12.js
@@ -0,0 +1,3 @@
+//12-. Imprime el array como una oración: Facebook, Google, Microsoft, Apple, IBM, Oracle y Amazon son grandes empresas de TI.
+
+console.log(`${itCompanies[0]}, ${itCompanies[1]}, ${itCompanies[2]}, ${itCompanies[3]}, ${itCompanies[4]}, ${itCompanies[5]} y ${itCompanies[6]} son grandes empresas de TI `)
