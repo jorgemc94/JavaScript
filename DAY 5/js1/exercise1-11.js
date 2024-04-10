0b39c5d433f3a4147d49644979dd1d0899697f49
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-10 13:18:43 +0200

    ADD day 5 exercise 1-11

---
+++ DAY 5/js1/exercise1-11.js
@@ -0,0 +1,9 @@
+//11-. Cambie el nombre de cada empresa a mayúsculas uno por uno e imprímalos.
+
+console.log(itCompanies[0].toUpperCase())
+console.log(itCompanies[1].toUpperCase())
+console.log(itCompanies[2].toUpperCase())
+console.log(itCompanies[3].toUpperCase())
+console.log(itCompanies[4].toUpperCase())
+console.log(itCompanies[5].toUpperCase())
+console.log(itCompanies[6].toUpperCase())
