e39d837a93c9d76e6141857f855d638b17ae2755
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-11 13:17:46 +0200

    ADD day 6 exercise 1-3

--- DAY 6/js1/exercise1-1.js
+++ DAY 6/js1/exercise1-1.js
@@ -1,6 +1,6 @@
 //1-. Itera de 0 a 10 usando el bucle for, haga lo mismo usando los bucles while y do while
 
 for (let i = 0; i <= 10; i++) {
-    const element = [i];
+    const element = i;
     console.log(element)
 }
---
+++ DAY 6/js1/exercise1-3.js
@@ -0,0 +1,7 @@
+//Itera de 0 a n usando el bucle for
+
+const n = prompt('Introduce un número:')
+for (let k = 0; k <= n; k++) {
+    const elementPrompt = k;
+    console.log(elementPrompt)
+}
