0db6f39c48bb6549d5a9ff1fb515b343d157d39a
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-11 13:55:22 +0200

    ADD day 6 exercise 1-7

---
+++ DAY 6/js1/exercise1-7.js
@@ -0,0 +1,11 @@
+//7-. Usa el bucle for para iterar de 0 a 100 e imprima solo números pares
+//8-. Usa el bucle for para iterar de 0 a 100 e imprima solo números impares
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`El número ${i} es par`)
+    } else {
+        console.log(`El número ${i} es impar`)
+    }
+    
+}
---
+++ DAY 6/js1/exercise1-8.js
@@ -0,0 +1,1 @@
+//8-. Usa el bucle for para iterar de 0 a 100 e imprima los solo números primos
