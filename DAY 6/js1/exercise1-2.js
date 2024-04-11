11c044f23a7b4ea6d0363328b2c4056e53c357ec
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-11 13:15:13 +0200

    ADD day 6 exercise 1-1 1-2

---
+++ DAY 6/js1/exercise1-2.js
@@ -0,0 +1,6 @@
+//2-. Itera 10 to 0 usando el bucle for, haga lo mismo usando los bucles while y do while
+
+for (let j = 10; j >=0; j--) {
+    const elementReverse = [j];
+    console.log(elementReverse)
+}
