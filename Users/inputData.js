6780d283d9fc67085b414d707ef20afe3b232f9a
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-17 18:47:20 +0200

    ADD exercise 4 comprobacion number

--- Users/inputData.js
+++ Users/inputData.js
@@ -13,10 +13,14 @@ const inputNumber = document.createElement('input');
 inputNumber.placeholder = 'Introduce un numero';
 inputNumber.addEventListener('change', (event) => {
 let userInput = event.target.value;
+if (!parseInt(userInput)) {
+    alert('Introduce un número')
+} else {
 let urlSplit = url.split('=');
 let urlReplace = urlSplit[1].replace('20',userInput)
 let urlNew = urlSplit[0] + '=' + urlReplace + '=' + urlSplit[2]
 console.log(urlNew)
+}
 
 })
 exercise4.appendChild(inputNumber);
