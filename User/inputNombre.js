ae0bd9fd5e0799ed137bce7a42bd34380884029b
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-17 09:57:00 +0200

    ADD title User

---
+++ User/inputNombre.js
@@ -0,0 +1,7 @@
+//3-. Crear un input que permita filtrar por nombre de la persona
+
+const exercise3 = document.getElementById('exercise3');
+
+const title3 = document.createElement('h4');
+title3.innerText = '3-. Crear un input que permita filtrar por nombre de la persona';
+exercise3.appendChild(title3);
---
+++ User/person.js
@@ -0,0 +1,5 @@
+//Crear la clase Personas
+
+class Person {
+    
+}
